7b76d852d66d94f7e422b0384e01523be4eae14c
Alexander-Agu <theonlyagu@gmail.com>
2024-11-14 14:07:41 +0200

    Re-styling the header section

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -4,42 +4,29 @@ import './header.css'
 function Header() {
   return (
     <header className='header'>
-        <div className="logo">
-          <a href="#">SenaX</a>
-        </div>
+      <div className="logo">
+        <a>SB</a>
+      </div>
 
-        <nav className='navbar'>
+      <nav className='navbar'>
+        <ul className='navList'>
+          <li className='navItem'>
+            <a href="#" className='navLink'>ATM</a>
+          </li>
 
-          <ul className='navList'>
+          <li className='navItem'>
+            <a href="#" className='navLink home'>SenaX Banking</a>
+          </li>
 
-            <li className='navItem'>
-              <a href="#" className='navLink'>
-                Home
-              </a>
-            </li>
+          <li className='navItem'>
+            <a href="#" className='navLink'>About</a>
+          </li>
+        </ul>
+      </nav>
 
-            <li className='navItem'>
-              <a href="#" className='navLink'>
-                Register
-              </a>
-            </li>
-            
-
-            <li className='navItem'>
-              <a href="#" className='navLink'>
-                ATM
-              </a>
-            </li>
-
-            <li className='navItem'>
-              <a href="#" className='navLink'>
-                About
-              </a>
-            </li>
-
-          </ul>
-
-        </nav>
+      <div className="join">
+        <a href="#">Register now</a>
+      </div>
     </header>
   )
 }
---
+++ frontend/src/components/hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './hero.css'
+
+function Hero() {
+  return (
+    <section className='hero'>
+
+    </section>
+  )
+}
+
+export default Hero
+
+/* 
+<p>
+    Welcome to SenaX Banking. We help you Manage your finances effortlessly. Our app offers secure, user-friendly features to help you bank with ease
+</p>
+
+<p>
+    SenaX Banking is designed to make your financial life simpler and more efficient.With our cutting-edge security measures, you can trust that your personal and financial information is always protected
+</p>
+*/
